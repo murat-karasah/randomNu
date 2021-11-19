feff4b6a8ee8a7948cfca52453400d285eaee534
Sabahci <Sabahci@DESKTOP-O6O0BMG>
2021-11-19 11:21:57 +0300

    Add project files.

---
+++ randomNu/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace randomNu
+{
+    class Program
+    {
+        static int tahmin;
+        static void Main(string[] args)
+        {
+            Random sayi = new Random();
+            int rnd = sayi.Next(0, 20);
+            Console.WriteLine("lütfen sayı giriniz");
+            int tahmin = Convert.ToInt32(Console.ReadLine());
+            int i = 0;
+            while (i<10)
+            {
+                
+                if (tahmin==rnd)
+                {
+                    Console.WriteLine("Tebrikler Doğru Tahmin");
+                    break;
+                }
+                else
+                {
+                    int kalan = 9 - i;
+                    Console.WriteLine("Yanlış Tahmin lütfen sayı giriniz Kalan tahmin hakkınız " + kalan);
+                     tahmin = Convert.ToInt32(Console.ReadLine());
+                    i++;
+                }
+               
+                if (i == 10)
+                {
+                    Console.WriteLine("Tahmin Hakkınız Bitti");
+                    break;
+                }
+                
+                
+            }
+        }
+    }
+}
